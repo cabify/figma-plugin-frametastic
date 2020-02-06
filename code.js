b4e0fab708cc7385344f2d96d64048153e2c7b18
basiclines <igonzaleznicolas@gmail.com>
2020-02-06 16:46:36 +0100

    Added id passing to code and basic duplication of frames and instances

--- code.js
+++ code.js
@@ -1,15 +1,28 @@
 figma.showUI(__html__);
 
+const X_SPACING = 100
+
+let DB = [
+    { name: 'Android', variations: [{ width: 320, height: 360 }, { width: 360, height: 360 }] },
+    { name: 'Banners', variations: [{ width: 540, height: 720 }, { width: 240, height: 480 }] }
+]
+
 function createStand(source, id) {
 
 }
 
 function createVariation(source, width, height) {
-
+    console.log(source, width, height)
+    source.forEach(node => {
+        let clone = (node.type === 'COMPONENT') ? node.createInstance() : node.clone();
+        clone.resize(width, height)
+        clone.x = node.x + node.width + X_SPACING
+        clone.y = node.y
+    })
 }
 
-function getSavedVariation(id) {
-
+function getSavedVariation(standId, variationId) {
+    return DB[standId].variations[variationId]
 }
 
 
@@ -20,16 +33,14 @@ figma.ui.onmessage = msg => {
 
     if (msg.type === 'run-stand') {
         alert('run stand')
-        // let id = msg.id
+        // let id = msg.standId
         // createStand(source, id)
     }
 
     if (msg.type === 'run-variation') {
-        alert('run variation')
-        // let id = msg.id
-        // let {width, height} = getSavedVariation(id)
-        // let source = figma.currentPage.selection
-        // createVariation(source, width, height)
+        let source = figma.currentPage.selection
+        let variation = getSavedVariation(msg.standId, msg.variationId)
+        createVariation(source, variation.width, variation.height)
     }
 
     // figma.closePlugin();
