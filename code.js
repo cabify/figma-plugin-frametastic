9856746044362cc65c381abeeaae2b645f13dc75
basiclines <igonzaleznicolas@gmail.com>
2020-02-06 13:08:45 +0100

    Added core functions

--- code.js
+++ code.js
@@ -1,24 +1,34 @@
-// This plugin will open a modal to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser enviroment (see documentation).
-
-// This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
+function createStand(source, id) {
+
+}
+
+function createVariation(source, width, height) {
+
+}
+
+function getSavedVariation(id) {
+
+}
+
+
 figma.ui.onmessage = msg => {
-// One way of distinguishing between different types of messages sent from
-// your HTML page is to use an object with a "type" property like this.
     if (msg.type === 'message-name') {
         alert('trigger')
     }
 
-// Make sure to close the plugin when you're done. Otherwise the plugin will
-// keep running, which shows the cancel button at the bottom of the screen.
+    if (msg.type === 'run-stand') {
+        let id = msg.id
+        createStand(source, id)
+    }
+
+    if (msg.type === 'run-variation') {
+        let id = msg.id
+        let {width, height} = getSavedVariation(id)
+        let source = figma.currentPage.selection
+        createVariation(source, width, height)
+    }
+
     figma.closePlugin();
 };
