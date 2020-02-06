1471609221ae28d9c29fe0b5a73f5130c11a6000
basiclines <igonzaleznicolas@gmail.com>
2020-02-06 17:30:03 +0100

    Added multiselection stand and variations creations

--- code.js
+++ code.js
@@ -7,40 +7,46 @@ let DB = [
     { name: 'Banners', variations: [{ width: 540, height: 720 }, { width: 240, height: 480 }] }
 ]
 
-function createStand(source, id) {
-
-}
-
-function createVariation(source, width, height) {
-    console.log(source, width, height)
-    source.forEach(node => {
-        let clone = (node.type === 'COMPONENT') ? node.createInstance() : node.clone();
-        clone.resize(width, height)
-        clone.x = node.x + node.width + X_SPACING
-        clone.y = node.y
+function createStand(node, id) {
+    let stand = getSavedStand(id)
+    let offset = 0
+    stand.variations.forEach((variation, index) => {
+        createVariation(node, variation.width, variation.height, offset)
+        offset = variation.width + X_SPACING
     })
 }
 
+function createVariation(node, width, height, offset) {
+    let clone = (node.type === 'COMPONENT') ? node.createInstance() : node.clone();
+    clone.resize(width, height)
+    clone.x = node.x + node.width + X_SPACING + (offset || 0)
+    clone.y = node.y
+}
+
+function getSavedStand(standId) {
+    return DB[standId]
+}
+
 function getSavedVariation(standId, variationId) {
-    return DB[standId].variations[variationId]
+    return getSavedStand(standId).variations[variationId]
 }
 
 
 figma.ui.onmessage = msg => {
-    if (msg.type === 'message-name') {
-        alert('trigger')
-    }
 
     if (msg.type === 'run-stand') {
-        alert('run stand')
-        // let id = msg.standId
-        // createStand(source, id)
+        let source = figma.currentPage.selection
+        source.forEach(node => {
+            createStand(node, msg.standId)
+        })
     }
 
     if (msg.type === 'run-variation') {
         let source = figma.currentPage.selection
         let variation = getSavedVariation(msg.standId, msg.variationId)
-        createVariation(source, variation.width, variation.height)
+        source.forEach(node => {
+            createVariation(node, variation.width, variation.height)
+        })
     }
 
     // figma.closePlugin();
